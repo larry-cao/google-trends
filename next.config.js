15a77b505f003f4aebac44b46ff47e669a65a84e
larry-cao <2372594332@qq.com>
2025-01-03 17:56:19 +0800

    chore: prepare for vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   experimental: {
     appDir: true,
   },
   typescript: {
     ignoreBuildErrors: false,
-  },
+  }
 }
 
 module.exports = nextConfig 
